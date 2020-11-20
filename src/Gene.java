8cd31ce234dfc449c07128bc406600d1902d9a35
d.dunnewold@live.com <Ro4#66hh>
2020-11-20 15:21:58 +0100

    Uitwerken gene

---
+++ src/Gene.java
@@ -0,0 +1,47 @@
+public class Gene implements Comparable <Gene>{
+
+private int gene_id;
+private String symbol;
+private String Chromosome_map_location;
+
+    public Gene(int gene_id, String symbol, String chromosome_map_location) {
+        this.gene_id = gene_id;
+        this.symbol = symbol;
+        Chromosome_map_location = chromosome_map_location;
+    }
+
+    public int getGene_id() {
+        return gene_id;
+    }
+
+    public void setGene_id(int gene_id) {
+        this.gene_id = gene_id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getChromosome_map_location() {
+        return Chromosome_map_location;
+    }
+
+    public void setChromosome_map_location(String chromosome_map_location) {
+        Chromosome_map_location = chromosome_map_location;
+    }
+//19q13.43
+    public int compareTo(Gene g){
+
+        if (this.Chromosome_map_location.charAt(1) > g.Chromosome_map_location) {
+            return +1;
+        } else if (this.Chromosome_map_location.equals(g.Chromosome_map_location)) {
+            return 0;
+        } else return -1; }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        String filename="C:\\Users\\ddunn\\IdeaProjects\\j2b2w2opdracht2\\src\\Homo_sapiens.gene_info";
+        ArrayList genes= readFile(filename);
+
+
     }
-}
+
+
+
+
+
+
+
+    public static ArrayList readFile(String filename) {
+        String line;
+        BufferedReader inFile = null;
+        try {
+            inFile = new BufferedReader(new FileReader(filename));
+            ArrayList<Gene> genes=new ArrayList<>();
+
+            while ((line = inFile.readLine()) != null) {
+
+                   String[] attributes=line.split("/t");
+                   int gene_id=Integer.parseInt(attributes[1]);
+                    String symbol= attributes[2];
+                    String Chromosome_map_location =attributes[7];
+                    genes.add(new Gene(gene_id,symbol,Chromosome_map_location));
+            }
+            return genes;
+
+
+        }
+        catch (IOException|NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,
+                    e.getMessage());
+            return null;
+        }
+        finally {
+            try {
+                inFile.close();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null,
+                        e.getMessage());
+            }
+        }
+}}
